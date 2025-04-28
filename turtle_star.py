444eae413436de5d4cdf298f61ae9434341c0b74
jInked-glitch <junao1219@gmail.com>
2025-04-28 14:54:32 +0200

    Update turtle_star.py
    Add turtle_star.py

--- turtle_star.py
+++ turtle_star.py
@@ -1,62 +1,31 @@
-import turtle
+
+# turtleモジュールを使う
+from turtle import Turtle, Screen
 import random
 
-from PIL import Image
-import colorgram
-
-from turtle import Turtle, Screen
-
-
-
-img = Image.open("image-sample.jpg")
-
-num_colors = 100
-colors = colorgram.extract("image-sample.jpg", num_colors)
-
-rgb_colors = []
-
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+list_colors = ["red", "blue", "green", "yellow", "purple", "orange"]
 
 def color_choice():
-    return random.choice(rgb_colors)
-# print(rgb_colors)
+    return random.choice(list_colors)
 
 tim = Turtle()
 turtle.colormode(255)
 tim.speed(0)
 tim.hideturtle()
-tim.penup()
+
 
 # スタート地点を調整する
 tim.setheading(150)
 tim.forward(300)
 tim.setheading(0)
 
-for row in range(10):
-    for col in range(10):
-        tim.dot(20, color_choice())
-        if col != 9:
-            tim.forward(50)
-        # 行が終わったら下に移動してスタート位置に戻る
-    tim.setheading(270)
-    tim.forward(50)
-    tim.setheading(180)
-    tim.forward(450)
-    tim.setheading(0)  # また右向きに戻す
+# 星を描くには5回線を引く（角度はちょっと特別）
+for line in range(5):
+    tim.color(color_choice())  # まず色を変える
+    tim.forward(100)           # それから前に進む
+    tim.right(144)
+        
+screen = Screen()
+# 最後は画面が消えないように待つ（クリック待ちなど）
+screen.exitonclick()
 
-    # # 1行描き終わったら下に移動
-    # tim.setheading(270)  # 下向き
-    # tim.forward(50)
-    # tim.setheading(180)  # 左向き
-    # for col in range(10):
-    #     tim.dot(20, color_choice())
-    #     if col != 9:
-    #         tim.forward(50)
-    # tim.setheading(270)  # 下向き
-    # tim.forward(50)
-    # tim.setheading(0)  # 右向きにリセット
